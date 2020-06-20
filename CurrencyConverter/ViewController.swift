9e87743158bdb751dc47823195bc84cfa07d5f55
Laurence Stone <larry.stone@gmail.com>
2020-06-19 20:47:25 -0500

    just getting started -- nothing works yet

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Laurence Stone on 6/19/20.
+//  Copyright © 2020 Laurence Stone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func fetchButton(_ sender: Any) {
+    }
+    
+    
+}
+
